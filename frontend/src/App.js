359f81461889419d3a8d8d505e1b40bfbd24aea2
Semyon <sema.stulnikov@mail.ru>
2024-02-02 23:36:56 +0300

    grid fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './slices/index.js';
 import Main from './pages/Main';
 
 const App = () => (
   <BrowserRouter>
-    <Provider store={store}>
-      <Main />
-    </Provider>
+    <Main />
   </BrowserRouter>
 
 );
--- frontend/src/slices/index.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import likeCatsReducer from './likeCatsSlice.js';
-
-export default configureStore({
-  reducer: {
-    likeCatsReducer,
-  },
-});
